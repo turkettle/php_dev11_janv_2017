aad73414d0f17f91066eca8ca3e1bb0d086c5f55
Aston Ecole <aston@example.com>
2017-01-19 13:38:05 +0100

    Amélioration.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
 
     require '../vendor/autoload.php';
+    // phpinfo();
+
+    $db_connection = new PDO('mysql:host=mysql.server.com;dbname=library', 'acme', 'paris');
+    
+    kint::dump($db_connection);
 ?>
