e30d9d291431b958c849c69b1c17f947fd50719d
Aston Ecole <aston@example.com>
2017-01-20 10:36:21 +0100

    Utilisation de l'ajax avec une API Rst.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
+// Test API Key:
+//     cee3bcb9-0c81-41ef-88ad-59a4a9ab79d6
+// Test keys are unmetered and return dummy holiday day.
+//
+//     Live API Key:
+//     ce7999d8-0112-4836-bfa8-5b2b9f389100
+// Limited to 500 calls per month, historical data only.
 
-var button = $('#reservBook');
+
+    var button = $('#reservBook');
 
 button.click(function(event) {
 
@@ -8,11 +16,19 @@ button.click(function(event) {
 
     $.ajax({
         method: 'GET',
-        url: 'http://localhost/ajax/add_reservation.php',
-        data: { info: reservInfo },
+        url: 'https://holidayapi.com/v1/holidays',
+        data: {
+            key: 'cee3bcb9-0c81-41ef-88ad-59a4a9ab79d6',
+            country: 'FR',
+            year: '2017'
+        },
     })
-        .success(function(message) {
-            $('#book-header').before(message);
+        .success(function(response) {
+            if (response.status == 200) {
+                $.each(response.holidays, function(index, holiday) {
+                    $('#book-header').after('<div>' + holiday[0].name + ' : ' + holiday[0].date + '</div>');
+                });
+            }
         });
 
 });
